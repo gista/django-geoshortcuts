cc7ba495ac5887b4f76df970583dcd35708090b9
Marcel Dancak <marcel.dancak@gista.sk>
2011-10-19 11:58:27 +0200

    * fixed bug in find_geom_field function (before it worked only if a geometry field was defined as last model's field)

--- geoshortcuts/__init__.py
+++ geoshortcuts/__init__.py
@@ -3,8 +3,6 @@ from django.contrib.gis.db.models.fields import GeometryField
 def find_geom_field(queryset):
 	"""Function returns geometry field name in model of query set. If doesn't exist, raise ValueError"""
 	for field in queryset.model._meta.fields:
-		geom_field = field.name if isinstance(field, GeometryField) else None
-	if geom_field is None:
-		raise ValueError
-	else:
+		geom_field = field.name if isinstance(field, GeometryField)
 		return geom_field
+	raise ValueError
